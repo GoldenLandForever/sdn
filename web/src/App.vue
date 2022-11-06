d103590e18d5a9e69d5073340c7a9fc2c31aac86
GoldenLandForever <108108205+GoldenLandForever@users.noreply.github.com>
2022-11-06 22:07:47 +0800

    Initial commit

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <a href="#" class="link-primary">Primary link</a>
+<a href="#" class="link-secondary">Secondary link</a>
+<a href="#" class="link-success">Success link</a>
+<a href="#" class="link-danger">Danger link</a>
+<a href="#" class="link-warning">Warning link</a>
+<a href="#" class="link-info">Info link</a>
+<a href="#" class="link-light">Light link</a>
+<a href="#" class="link-dark">Dark link</a>
+</template>
+<script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"  
+export default{
+  name:"App",
+  setup:() =>{
+
+  }
+}
+</script>
+<style>
+</style>
