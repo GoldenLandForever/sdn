5885330d1bc4e3425501f874c721d6630360c6d7
GoldenLandForever <108108205+GoldenLandForever@users.noreply.github.com>
2022-11-06 22:13:48 +0800

    test

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,9 +4,6 @@
 <a href="#" class="link-success">Success link</a>
 <a href="#" class="link-danger">Danger link</a>
 <a href="#" class="link-warning">Warning link</a>
-<a href="#" class="link-info">Info link</a>
-<a href="#" class="link-light">Light link</a>
-<a href="#" class="link-dark">Dark link</a>
 </template>
 <script>
 import "bootstrap/dist/css/bootstrap.min.css"
